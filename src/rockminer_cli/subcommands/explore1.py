fdab5a5bc47b2851d1f40ef0a69ea8777d14b8bd
David Erb <david.erb@diamond.ac.uk>
2023-04-05 15:49:23 +0000

    prototype query working

--- src/rockminer_cli/subcommands/explore1.py
+++ src/rockminer_cli/subcommands/explore1.py
@@ -2,9 +2,11 @@ import asyncio
 
 # Use standard logging in this module.
 import logging
+from collections import OrderedDict
 
 import pytds
 from dls_utilpack.describe import describe
+from dls_utilpack.explain import explain
 
 # Xchembku client context.
 from xchembku_api.datafaces.context import Context as XchembkuDatafacesContext
@@ -58,31 +60,99 @@ class Explore1(Base):
         mssql = configuration["rockminer_mssql"]
 
         # connect to the RockMaker database
-        conn = pytds.connect(
+        self.__connection = pytds.connect(
             mssql["server"],
             mssql["database"],
             mssql["username"],
             mssql["password"],
         )
-        c = conn.cursor()
 
-        # find the directory names corresponding to plate types for all entries in the XChem folder
-        # XChem
-        # |- Plate Type (e.g. SwissSci3D)
-        #    |- Barcode
-        #       |- Data
+        # Plate's treenode is "ExperimentPlate".
+        # Parent of ExperimentPlate is "Experiment", aka visit
+        # Parent of Experiment is "Project", aka plate type.
+        # Parent of Project is "ProjectsFolder", we only care about "XChem"
+        # Get all xchem barcodes and the associated experiment name.
+        records = self.query(
+            "SELECT"
+            " Plate.ID AS id,"
+            " Plate.Barcode AS barcode,"
+            " experiment_node.Name AS visit"
+            " FROM Plate"
+            " JOIN Experiment ON experiment.ID = plate.experimentID"
+            " JOIN TreeNode AS experiment_node ON experiment_node.ID = Experiment.TreeNodeID"
+            " JOIN TreeNode AS plate_type_node ON plate_type_node.ID = experiment_node.ParentID"
+            " JOIN TreeNode AS projects_folder_node ON projects_folder_node.ID = plate_type_node.ParentID"
+            " WHERE projects_folder_node.Name = 'xchem'"
+            " AND plate_type_node.NAME IN ('SWISSci_3drop')"
+        )
+
+        for record in records:
+            logger.debug(
+                f"plate {record['id']} barcode {record['barcode']}, visit {record['visit']}"
+            )
+            # logger.debug(
+            #     "%s=%s %s=%s %s=%s %s=%s"
+            #     % (
+            #         row[6],
+            #         row[7],
+            #         row[4],
+            #         row[5],
+            #         row[2],
+            #         row[3],
+            #         row[0],
+            #         row[1],
+            #     )
+            # )
+
+        return
+
+        # Plate's treenode is "ExperimentPlate".
+        # Parent of ExperimentPlate is "Experiment".
+        # Parent of Experiment is "Project", aka plate type.
+        # Parent of Project is "ProjectsFolder", we only care about "XChem"
+        # Get all xchem barcodes and the associated experiment name.
         c.execute(
-            "SELECT TN3.Name as 'Name' From Plate "
+            "SELECT DISTINCT "
+            "TN1.Type, "
+            "TN1.Name, "
+            "TN2.Type, "
+            "TN2.Name, "
+            "TN3.Type, "
+            "TN3.Name, "
+            "TN4.Type, "
+            "TN4.Name "
+            "From Plate "
             "INNER JOIN TreeNode TN1 ON Plate.TreeNodeID = TN1.ID "
             "INNER JOIN TreeNode TN2 ON TN1.ParentID = TN2.ID "
             "INNER JOIN TreeNode TN3 ON TN2.ParentID = TN3.ID "
             "INNER JOIN TreeNode TN4 ON TN3.ParentID = TN4.ID "
-            "where TN4.Name='Xchem'"
+            "WHERE TN4.Name IN ('Xchem', 'XChem') "
+            "AND TN3.NAME IN ('SWISSci_3drop')"
         )
+
+        for row in c.fetchall():
+            project = row[5]
+            experiment = row[3]
+            logger.debug(f"plate type {project}, visit {experiment}")
+            # logger.debug(
+            #     "%s=%s %s=%s %s=%s %s=%s"
+            #     % (
+            #         row[6],
+            #         row[7],
+            #         row[4],
+            #         row[5],
+            #         row[2],
+            #         row[3],
+            #         row[0],
+            #         row[1],
+            #     )
+            # )
+
+        return
 
         plate_types = []
         for row in c.fetchall():
-            plate_types.append(str(row[0]))
+            plate_types.append(str(row[4]))
         # get all plate types
         plate_types = list(set(plate_types))
         # lists to hold barcode
@@ -154,6 +224,40 @@ class Explore1(Base):
                 break
 
             break
+
+    # ----------------------------------------------------------------------------------------
+    def query(self, sql, subs=None, why=None):
+
+        if subs is None:
+            subs = []
+
+        try:
+            cursor = self.__connection.cursor()
+            cursor.execute(sql, subs)
+            rows = cursor.fetchall()
+            cols = []
+            for col in cursor.description:
+                cols.append(col[0])
+
+            if why is None:
+                logger.debug("%d records from: %s" % (len(rows), sql))
+            else:
+                logger.debug("%d records from %s: %s" % (len(rows), why, sql))
+            records = []
+            for row in rows:
+                record = OrderedDict()
+                for index, col in enumerate(cols):
+                    record[col] = row[index]
+                records.append(record)
+            return records
+        except Exception as exception:
+            if why is None:
+                raise RuntimeError(explain(exception, f"executing {sql}"))
+            else:
+                raise RuntimeError(explain(exception, f"executing {why}: {sql}"))
+        finally:
+            if cursor is not None:
+                cursor.close()
 
     # ----------------------------------------------------------
     def add_arguments(parser):
